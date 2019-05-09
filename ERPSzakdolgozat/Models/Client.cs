b636d8fda78d686a20cfd0cbf9469e589be6680b
Gergely Horvath <gergelyzsolt.hrv@gmail.com>
2019-05-09 16:34:36 +0200

    Client model and seed

--- ERPSzakdolgozat/Models/Client.cs
+++ ERPSzakdolgozat/Models/Client.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ERPSzakdolgozat.Models
 {
@@ -9,6 +10,30 @@ namespace ERPSzakdolgozat.Models
 
 		[Required]
 		public string ClientName { get; set; }
+
+		public string Email { get; set; }
+
+		public string Phone { get; set; }
+
+		public string Mobile { get; set; }
+
+		public string ContactName { get; set; }
+
+		public string Country { get; set; }
+
+		public string ZIP { get; set; }
+
+		public string City { get; set; }
+
+		public string Street { get; set; }
+
+		[NotMapped] // this means the field is not mapped into a table-field in the DB
+		public string Address {
+			get => Country + " - " + ZIP + " " + City + ", " + Street;
+			set { }
+		}
+
+		public string TaxNumber { get; set; }
 
 		public bool Active { get; set; }
 	}
--- ERPSzakdolgozat/Models/ERPDBContext.cs
+++ ERPSzakdolgozat/Models/ERPDBContext.cs
@@ -27,184 +27,229 @@ namespace ERPSzakdolgozat.Models
 				.HasForeignKey(e => e.EmployeeId);
 
 			// Add at least one record of every Model
-			modelBuilder.Entity<Employee>().HasData(new Employee
-			{
-				Id = 1,
-				CreatedDate = DateTime.Now,
-				ModifiedDate = DateTime.Now,
-				Active = true,
-				CompanyIdentifier = "XY0001",
-				DateOfBirth = new DateTime(1990, 12, 26),
-				Email = "kiskutya@corgik.hu",
-				LeaderId = 2,
-				Mobile = "+36901234567",
-				JoinedOn = DateTime.Now.AddYears(-1),
-				EmployeeName = "Kis Kutya",
-				TeamId = 1,
-				SameAddress = false,
-				HomeZIP = "2400",
-				HomeCountry = "Magyarország",
-				HomeCity = "Dunaújváros",
-				HomeStreet = "Táncsics M. u. 28.",
-				IsLeader = false,
-				RoleId = 1
-			},
-			new Employee
-			{
-				Id = 2,
-				CreatedDate = DateTime.Now,
-				ModifiedDate = DateTime.Now,
-				Active = true,
-				CompanyIdentifier = "ZZ9999",
-				DateOfBirth = new DateTime(1990, 12, 25),
-				Email = "nagykutya@husky.hu",
-				LeaderId = 2,
-				Mobile = "+36909999999",
-				JoinedOn = DateTime.Now.AddYears(-1),
-				EmployeeName = "Nagy Kutya",
-				TeamId = 1,
-				SameAddress = true,
-				HomeZIP = "2400",
-				HomeCountry = "Magyarország",
-				HomeCity = "Dunaújváros",
-				HomeStreet = "Táncsics M. u. 1/a.",
-				MailZIP = "2400",
-				MailCountry = "Magyarország",
-				MailCity = "Dunaújváros",
-				MailStreet = "Táncsics M. u. 1/a.",
-				IsLeader = true,
-				RoleId = 2
-			});
+			modelBuilder.Entity<Employee>().HasData(
+				new Employee
+				{
+					Id = 1,
+					CreatedDate = DateTime.Now,
+					ModifiedDate = DateTime.Now,
+					Active = true,
+					CompanyIdentifier = "XY0001",
+					DateOfBirth = new DateTime(1990, 12, 26),
+					Email = "kiskutya@corgik.hu",
+					LeaderId = 2,
+					Mobile = "+36901234567",
+					JoinedOn = DateTime.Now.AddYears(-1),
+					EmployeeName = "Kis Kutya",
+					TeamId = 1,
+					SameAddress = false,
+					HomeZIP = "2400",
+					HomeCountry = "Magyarország",
+					HomeCity = "Dunaújváros",
+					HomeStreet = "Táncsics M. u. 28.",
+					IsLeader = false,
+					RoleId = 1
+				},
+				new Employee
+				{
+					Id = 2,
+					CreatedDate = DateTime.Now,
+					ModifiedDate = DateTime.Now,
+					Active = true,
+					CompanyIdentifier = "ZZ9999",
+					DateOfBirth = new DateTime(1990, 12, 25),
+					Email = "nagykutya@husky.hu",
+					LeaderId = 2,
+					Mobile = "+36909999999",
+					JoinedOn = DateTime.Now.AddYears(-1),
+					EmployeeName = "Nagy Kutya",
+					TeamId = 1,
+					SameAddress = true,
+					HomeZIP = "2400",
+					HomeCountry = "Magyarország",
+					HomeCity = "Dunaújváros",
+					HomeStreet = "Táncsics M. u. 1/a.",
+					MailZIP = "2400",
+					MailCountry = "Magyarország",
+					MailCity = "Dunaújváros",
+					MailStreet = "Táncsics M. u. 1/a.",
+					IsLeader = true,
+					RoleId = 2
+				});
 
-			modelBuilder.Entity<EmployeeFinancial>().HasData(new EmployeeFinancial
-			{
-				Id = 1,
-				CreatedDate = DateTime.Now,
-				ModifiedDate = DateTime.Now,
-				ActiveFrom = DateTime.Now.AddMonths(-2),
-				Bonus = 50,
-				Cafeteria = 200,
-				CurrencyId = 2,
-				EmployeeId = 1,
-				GrossSalary = 400,
-				WorkHours = 8
-			},
-			new EmployeeFinancial
-			{
-				Id = 2,
-				CreatedDate = DateTime.Now,
-				ModifiedDate = DateTime.Now,
-				ActiveFrom = DateTime.Now.AddMonths(-1),
-				Bonus = 1000,
-				Cafeteria = 0,
-				CurrencyId = 2,
-				EmployeeId = 1,
-				GrossSalary = 10,
-				WorkHours = 25
-			},
-			new EmployeeFinancial
-			{
-				Id = 3,
-				CreatedDate = DateTime.Now,
-				ModifiedDate = DateTime.Now,
-				ActiveFrom = DateTime.Now.AddMonths(-1),
-				Bonus = 0,
-				Cafeteria = 1,
-				CurrencyId = 2,
-				EmployeeId = 2,
-				GrossSalary = 99999,
-				WorkHours = 2
-			});
+			modelBuilder.Entity<EmployeeFinancial>().HasData(
+				new EmployeeFinancial
+				{
+					Id = 1,
+					CreatedDate = DateTime.Now,
+					ModifiedDate = DateTime.Now,
+					ActiveFrom = DateTime.Now.AddMonths(-2),
+					Bonus = 50,
+					Cafeteria = 200,
+					CurrencyId = 2,
+					EmployeeId = 1,
+					GrossSalary = 400,
+					WorkHours = 8
+				},
+				new EmployeeFinancial
+				{
+					Id = 2,
+					CreatedDate = DateTime.Now,
+					ModifiedDate = DateTime.Now,
+					ActiveFrom = DateTime.Now.AddMonths(-1),
+					Bonus = 1000,
+					Cafeteria = 0,
+					CurrencyId = 2,
+					EmployeeId = 1,
+					GrossSalary = 10,
+					WorkHours = 25
+				},
+				new EmployeeFinancial
+				{
+					Id = 3,
+					CreatedDate = DateTime.Now,
+					ModifiedDate = DateTime.Now,
+					ActiveFrom = DateTime.Now.AddMonths(-1),
+					Bonus = 0,
+					Cafeteria = 1,
+					CurrencyId = 2,
+					EmployeeId = 2,
+					GrossSalary = 99999,
+					WorkHours = 2
+				});
 
-			modelBuilder.Entity<Currency>().HasData(new Currency
-			{
-				Id = 1,
-				CreatedDate = DateTime.Now,
-				ModifiedDate = DateTime.Now,
-				ExchangeValue = 1,
-				InYear = 2019,
-				CurrencyName = "HUF"
-			},
-			new Currency
-			{
-				Id = 2,
-				CreatedDate = DateTime.Now,
-				ModifiedDate = DateTime.Now,
-				ExchangeValue = 300,
-				InYear = 2019,
-				CurrencyName = "USD"
-			});
+			modelBuilder.Entity<Currency>().HasData(
+				new Currency
+				{
+					Id = 1,
+					CreatedDate = DateTime.Now,
+					ModifiedDate = DateTime.Now,
+					ExchangeValue = 1,
+					InYear = 2019,
+					CurrencyName = "HUF"
+				},
+				new Currency
+				{
+					Id = 2,
+					CreatedDate = DateTime.Now,
+					ModifiedDate = DateTime.Now,
+					ExchangeValue = 300,
+					InYear = 2019,
+					CurrencyName = "USD"
+				});
 
-			modelBuilder.Entity<Role>().HasData(new Role
-			{
-				Id = 1,
-				CreatedDate = DateTime.Now,
-				ModifiedDate = DateTime.Now,
-				IsSelectable = true,
-				RoleName = "Developer"
-			},
-			new Role
-			{
-				Id = 2,
-				CreatedDate = DateTime.Now,
-				ModifiedDate = DateTime.Now,
-				IsSelectable = true,
-				RoleName = "Tester"
-			},
-			new Role
-			{
-				Id = 3,
-				CreatedDate = DateTime.Now,
-				ModifiedDate = DateTime.Now,
-				IsSelectable = true,
-				RoleName = "Manager"
-			});
+			modelBuilder.Entity<Role>().HasData(
+				new Role
+				{
+					Id = 1,
+					CreatedDate = DateTime.Now,
+					ModifiedDate = DateTime.Now,
+					IsSelectable = true,
+					RoleName = "Developer"
+				},
+				new Role
+				{
+					Id = 2,
+					CreatedDate = DateTime.Now,
+					ModifiedDate = DateTime.Now,
+					IsSelectable = true,
+					RoleName = "Tester"
+				},
+				new Role
+				{
+					Id = 3,
+					CreatedDate = DateTime.Now,
+					ModifiedDate = DateTime.Now,
+					IsSelectable = true,
+					RoleName = "Manager"
+				});
 
-			modelBuilder.Entity<SkillLevel>().HasData(new SkillLevel
-			{
-				Id = 1,
-				CreatedDate = DateTime.Now,
-				ModifiedDate = DateTime.Now,
-				IsSelectable = true,
-				SkillLevelName = "Junior"
-			},
-			new SkillLevel
-			{
-				Id = 2,
-				CreatedDate = DateTime.Now,
-				ModifiedDate = DateTime.Now,
-				IsSelectable = true,
-				SkillLevelName = "Senior"
-			},
-			new SkillLevel
-			{
-				Id = 3,
-				CreatedDate = DateTime.Now,
-				ModifiedDate = DateTime.Now,
-				IsSelectable = false,
-				SkillLevelName = "God"
-			});
+			modelBuilder.Entity<SkillLevel>().HasData(
+				new SkillLevel
+				{
+					Id = 1,
+					CreatedDate = DateTime.Now,
+					ModifiedDate = DateTime.Now,
+					IsSelectable = true,
+					SkillLevelName = "Junior"
+				},
+				new SkillLevel
+				{
+					Id = 2,
+					CreatedDate = DateTime.Now,
+					ModifiedDate = DateTime.Now,
+					IsSelectable = true,
+					SkillLevelName = "Senior"
+				},
+				new SkillLevel
+				{
+					Id = 3,
+					CreatedDate = DateTime.Now,
+					ModifiedDate = DateTime.Now,
+					IsSelectable = false,
+					SkillLevelName = "God"
+				});
 
-			modelBuilder.Entity<Team>().HasData(new Team
-			{
-				Id = 1,
-				CreatedDate = DateTime.Now,
-				ModifiedDate = DateTime.Now,
-				Active = true,
-				TeamCode = "BTE",
-				TeamName = "Best Team Ever",
-				UnitId = 1
-			});
+			modelBuilder.Entity<Team>().HasData(
+				new Team
+				{
+					Id = 1,
+					CreatedDate = DateTime.Now,
+					ModifiedDate = DateTime.Now,
+					Active = true,
+					TeamCode = "BTE",
+					TeamName = "Best Team Ever",
+					UnitId = 1
+				});
 
-			modelBuilder.Entity<Unit>().HasData(new Unit
-			{
-				Id = 1,
-				CreatedDate = DateTime.Now,
-				ModifiedDate = DateTime.Now,
-				Active = true,
-				UnitCode = "BUE",
-				UnitName = "Best Unit Ever"
-			});
+			modelBuilder.Entity<Unit>().HasData(
+				new Unit
+				{
+					Id = 1,
+					CreatedDate = DateTime.Now,
+					ModifiedDate = DateTime.Now,
+					Active = true,
+					UnitCode = "BUE",
+					UnitName = "Best Unit Ever"
+				});
+
+			modelBuilder.Entity<Client>().HasData(
+				new Client
+				{
+					Id = 1,
+					CreatedDate = DateTime.Now,
+					ModifiedDate = DateTime.Now,
+					Active = true,
+					ClientId = "CLIENT001",
+					City = "Nagylackunháza",
+					ClientName = "Kliens neve",
+					ContactName = "Helybenjáró Kelemen",
+					Country = "Mexikó",
+					Email = "kelemen@itt.hu",
+					Mobile = "06901112222",
+					Phone = "061987987",
+					Street = "Nagymama u. 99.",
+					TaxNumber = "554-4444554-222",
+					ZIP = "1234"
+				},
+				new Client
+				{
+					Id = 2,
+					CreatedDate = DateTime.Now,
+					ModifiedDate = DateTime.Now,
+					Active = true,
+					ClientId = "CLIENT002",
+					City = "Kistejföl",
+					ClientName = "Nem is a neve",
+					ContactName = "Helybenjáró Kelemen",
+					Country = "Oroszország",
+					Email = "kelemen@itt_is.hu",
+					Mobile = "06901112222",
+					Phone = "061987988",
+					Street = "Papa u. 1.",
+					TaxNumber = "52254-4124-2222322",
+					ZIP = "4321"
+				});
 
 			//modelBuilder.Entity<Post>(entity =>
 			//{
